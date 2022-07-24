1f9738d9d5a48798f02f337f49f3991c57c715b8
aalassi <aalassi@yahoo.com>
2022-07-24 14:52:39 +0200

    first commit

---
+++ assests/js/javascript.js
@@ -0,0 +1,96 @@
+
+
+
+function confirmDEL(){
+   if (confirm("bist du sicher, dass du alle Felder löschen willst ?")) {
+   return true;
+   }
+   return false;
+   }
+
+
+
+
+
+/*  function sorryFAKE() {
+   alert("Sorry, es ist ein FAKE FORMULAR");
+   return false
+ }
+ */
+
+/*  function sorryFAKE() {
+  if (confirm("Sorry, es ist ein FAKE FORMULAR, sicher fillst doch fortfahren?")) {
+    return true;
+  }
+  return false;
+  } */
+
+   function sorryFAKE() {
+   alert("Sorry, es ist ein FAKE FORMULAR");
+   event.preventDefault();
+   /*  window.history.back(); */
+ }
+
+
+
+
+
+
+
+ let calcScrollValue = () => {
+   let scrollProgress = document.getElementById("progress");
+   let progressValue = document.getElementById("progress-value");
+   let pos = document.documentElement.scrollTop;
+   let calcHeight =
+     document.documentElement.scrollHeight -
+     document.documentElement.clientHeight;
+   let scrollValue = Math.round((pos * 100) / calcHeight);
+   if (pos > 100) {
+     scrollProgress.style.display = "grid";
+   } else {
+     scrollProgress.style.display = "none";
+   }
+   scrollProgress.addEventListener("click", () => {
+     document.documentElement.scrollTop = 0;
+   });
+   scrollProgress.style.background = `conic-gradient(#5682bc ${scrollValue}%, #d7d7d7 ${scrollValue}%)`;
+ };
+ 
+ window.onscroll = calcScrollValue;
+ window.onload = calcScrollValue;
+ 
+
+
+
+
+
+ let revealerpoint = 150;
+window.addEventListener('scroll', reveal);
+reveal();
+
+function reveal() {
+    console.log("scrolling");
+    let revealers = document.querySelectorAll('.fade1');
+    for (let i = 0; i < revealers.length; i++) {
+        let windowheight = window.innerHeight;
+        let revealertop = revealers[i].getBoundingClientRect().top;
+      
+        if (revealertop < windowheight - revealerpoint) {
+            revealers[i].classList.add('fade-in')
+        } 
+           
+       
+        
+        
+    } 
+};
+
+
+
+
+
+
+
+
+
+
